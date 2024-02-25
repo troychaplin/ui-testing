cb92382c0e8071efdc2a1945faba7ce00d928cfb
troychaplin <troy.chaplin@gmail.com>
2024-02-25 23:55:50 +0000

    Deploying to gh-pages from @ troychaplin/ui-testing@f10e0c8bdc95947039f1ffc4297afb3294ef50fa 🚀

---
+++ assets/WideImage.stories-CJvZuYSI.js
@@ -0,0 +1,87 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as d}from"./Main-CJHjqGYv.js";import{S as c}from"./Section-CVJtbDHu.js";import{N as i,b as l,a as g}from"./NavData-BWKADDSs.js";import{F as x}from"./FooterBasic-BCWxiIU6.js";import{B as a}from"./Button-Br6DnAHj.js";import{H as t}from"./HeroTextImage-D1wdjrPn.js";import{W as o}from"./WideImage-C9cICKbU.js";import{B as h}from"./ButtonGroup-BqR3XMcH.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./globalClasses-GmQrI9Vi.js";import"./index-CezNtdIE.js";import"./Avatar-RY1zWvJc.js";import"./optionClasses-Chq8FjL-.js";import"./FooterLogoLinks-Cg8MhTHA.js";import"./Icon-BykHp66m.js";import"./ChevronDownIcon-DCf8Nrl_.js";const D={title:"Examples/Projects/cutheme"},s=`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus
+vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse
+condimentum magna vel orci vulputate, eget vulputate neque porttitor.`,n=()=>e.jsxs(e.Fragment,{children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet tortor pellentesque, posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel orci vulputate, eget vulputate neque porttitor. Suspendisse euismod, urna et gravida volutpat, tortor risus vehicula nisl, in vulputate lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus. Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis, quis pulvinar metus sapien in urna."}),e.jsx("p",{children:"Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem non. Molestiae quas dolores accusamus in. Praesent quis ligula quis nulla malesuada tempor."})]}),r={render:()=>e.jsxs(e.Fragment,{children:[e.jsxs(i,{navType:"secondary",children:[e.jsx(i.Logo,{title:"Web Services",link:"https://carleton.ca/webservices"}),e.jsx(i.Aside,{menu:l}),e.jsxs(i.Secondary,{children:[e.jsx(i.Menu,{menu:g}),e.jsx(i.Aside,{menu:l})]})]}),e.jsx(d,{children:e.jsxs(c,{hasProse:!0,children:[e.jsx(o,{headerType:"h1",image:"https://picsum.photos/1600/700",title:"Wide image block with buttons and background image",children:e.jsxs(h,{children:[e.jsx(a,{title:"Apply Now"}),e.jsx(a,{color:"white",isType:"outline",title:"Request Information"})]})}),e.jsx(n,{}),e.jsxs(o,{title:"Subscribe to Newsletter",headerType:"h1",isType:"dark",children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec tortor eu quam ultricies pellentesque et quis odio. Etiam sit amet ex commodo, scelerisque turpis ullamcorper, sollicitudin lacus."}),e.jsx(o.Signup,{buttonText:"Subscribe"})]}),e.jsx(n,{}),e.jsxs(t,{children:[e.jsx(t.Content,{title:"With image right and left angle",paragraph:s,hasMediaCol:!0,children:e.jsxs("div",{className:"flex flex-wrap gap-4 buttons md:gap-6",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})}),e.jsx(t.Media,{image:"https://picsum.photos/400/266",angle:"left"})]}),e.jsx(n,{}),e.jsxs(t,{reverse:!0,children:[e.jsx(t.Content,{title:"Website and Application Development",paragraph:s,hasMediaCol:!0,children:e.jsxs("div",{className:"flex flex-wrap gap-4 buttons md:gap-6",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})}),e.jsx(t.Media,{image:"https://picsum.photos/400/266",angle:"left"})]}),e.jsx(n,{}),e.jsxs(t,{children:[e.jsx(t.Content,{title:"Website and Application Development",paragraph:s,hasMediaCol:!0,children:e.jsxs("div",{className:"flex flex-wrap gap-4 buttons md:gap-6",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})}),e.jsx(t.Media,{image:"https://picsum.photos/400/266",angle:"dual"})]}),e.jsx(n,{}),e.jsxs(t,{children:[e.jsx(t.Content,{title:"Website and Application Development",paragraph:s,hasMediaCol:!0,children:e.jsxs("div",{className:"flex flex-wrap gap-4 buttons md:gap-6",children:[e.jsx(a,{title:"Primary"}),e.jsx(a,{title:"Secondary",color:"grey"})]})}),e.jsx(t.Media,{image:"https://picsum.photos/400/266",angle:"none"})]}),e.jsx(n,{})]})}),e.jsx(x,{})]})};var m,u,p;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => <>
+      <Nav navType="secondary">
+        <Nav.Logo title="Web Services" link="https://carleton.ca/webservices" />
+        <Nav.Aside menu={NavAsideData} />
+        <Nav.Secondary>
+          <Nav.Menu menu={NavDataSingle} />
+          <Nav.Aside menu={NavAsideData} />
+        </Nav.Secondary>
+      </Nav>
+
+      <Main>
+        <Section hasProse>
+          <WideImage headerType="h1" image="https://picsum.photos/1600/700" title="Wide image block with buttons and background image">
+            <ButtonGroup>
+              <Button title="Apply Now" />
+              <Button color="white" isType="outline" title="Request Information" />
+            </ButtonGroup>
+          </WideImage>
+
+          <DoublePara />
+
+          <WideImage title="Subscribe to Newsletter" headerType="h1" isType="dark">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec tortor eu quam ultricies
+              pellentesque et quis odio. Etiam sit amet ex commodo, scelerisque turpis ullamcorper, sollicitudin lacus.
+            </p>
+            <WideImage.Signup buttonText="Subscribe" />
+          </WideImage>
+
+          <DoublePara />
+
+          <HeroTextImage>
+            <HeroTextImage.Content title="With image right and left angle" paragraph={heroPara} hasMediaCol>
+              <div className="flex flex-wrap gap-4 buttons md:gap-6">
+                <Button title="Primary" />
+                <Button title="Secondary" color="grey" />
+              </div>
+            </HeroTextImage.Content>
+            <HeroTextImage.Media image="https://picsum.photos/400/266" angle="left" />
+          </HeroTextImage>
+
+          <DoublePara />
+
+          <HeroTextImage reverse>
+            <HeroTextImage.Content title="Website and Application Development" paragraph={heroPara} hasMediaCol>
+              <div className="flex flex-wrap gap-4 buttons md:gap-6">
+                <Button title="Primary" />
+                <Button title="Secondary" color="grey" />
+              </div>
+            </HeroTextImage.Content>
+            <HeroTextImage.Media image="https://picsum.photos/400/266" angle="left" />
+          </HeroTextImage>
+
+          <DoublePara />
+
+          <HeroTextImage>
+            <HeroTextImage.Content title="Website and Application Development" paragraph={heroPara} hasMediaCol>
+              <div className="flex flex-wrap gap-4 buttons md:gap-6">
+                <Button title="Primary" />
+                <Button title="Secondary" color="grey" />
+              </div>
+            </HeroTextImage.Content>
+            <HeroTextImage.Media image="https://picsum.photos/400/266" angle="dual" />
+          </HeroTextImage>
+
+          <DoublePara />
+
+          <HeroTextImage>
+            <HeroTextImage.Content title="Website and Application Development" paragraph={heroPara} hasMediaCol>
+              <div className="flex flex-wrap gap-4 buttons md:gap-6">
+                <Button title="Primary" />
+                <Button title="Secondary" color="grey" />
+              </div>
+            </HeroTextImage.Content>
+            <HeroTextImage.Media image="https://picsum.photos/400/266" angle="none" />
+          </HeroTextImage>
+
+          <DoublePara />
+        </Section>
+      </Main>
+
+      <FooterBasic />
+    </>
+}`,...(p=(u=r.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};const k=["WideImageBanner"];export{r as WideImageBanner,k as __namedExportsOrder,D as default};
