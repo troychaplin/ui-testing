ee14ab1a3ac7be3830fb1ea049562f39c95c45ec
Troy Chaplin <troy.chaplin@gmail.com>
2024-02-25 15:51:30 -0500

    Initial commit message

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./lib/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'ui-black': {
+          DEFAULT: '#000000',
+          50: '#f6f6f6',
+          100: '#e7e7e7',
+          200: '#d1d1d1',
+          300: '#b0b0b0',
+          400: '#888888',
+          500: '#6d6d6d',
+          600: '#5d5d5d',
+          700: '#4f4f4f',
+          800: '#454545',
+          900: '#3d3d3d',
+          950: '#000000',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
