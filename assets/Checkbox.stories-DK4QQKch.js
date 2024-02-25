641392ead5c0a74a56cf369a7a6d2570680afe3a
troychaplin <troy.chaplin@gmail.com>
2024-02-25 23:52:03 +0000

    Deploying to gh-pages from @ troychaplin/ui-testing@47d77d258f8a68f52006e2c4cf34a47bcc0e6853 🚀

---
+++ assets/Checkbox.stories-DK4QQKch.js
@@ -0,0 +1,9 @@
+import{j as r}from"./jsx-runtime-CKrituN3.js";import{F as e}from"./Form-CUY0XveS.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./XMarkIcon-iOOrpGul.js";import"./Button-Br6DnAHj.js";import"./Icon-BykHp66m.js";import"./optionClasses-Chq8FjL-.js";import"./ChevronDownIcon-DCf8Nrl_.js";import"./Calendar-DwcNFNf3.js";import"./startOfToday-C7FXxczV.js";import"./parse-BfiLtjV-.js";const k={title:"Components/Forms/Checkbox",component:e,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},a={render:()=>r.jsx(e,{onSubmit:()=>{},schema:{},initialValues:s,children:r.jsx(e.Checkbox,{name:"favcharacters",label:"Who are your favourite 90s sitcom characters?",options:[{name:"name",label:"name",id:2}]})})},s={firstname:"ish",description:" sample description",regularviewer:"2",location:"Carleton University, Colonel By Drive, Ottawa, ON, Canada",favsitcom:"familymatter",favcharacters:["kellykapowski","carltonbanks"]};var o,t,i;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <Form onSubmit={() => undefined} schema={{}} initialValues={InitialValues}>
+      <Form.Checkbox name="favcharacters" label="Who are your favourite 90s sitcom characters?" options={[{
+      name: 'name',
+      label: 'name',
+      id: 2
+    }]} />
+    </Form>
+}`,...(i=(t=a.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const C=["Default"];export{a as Default,C as __namedExportsOrder,k as default};
---
+++ assets/Radio.stories-CxOoKiqn.js
@@ -0,0 +1,8 @@
+import{j as o}from"./jsx-runtime-CKrituN3.js";import{F as r}from"./Form-CUY0XveS.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./XMarkIcon-iOOrpGul.js";import"./Button-Br6DnAHj.js";import"./Icon-BykHp66m.js";import"./optionClasses-Chq8FjL-.js";import"./ChevronDownIcon-DCf8Nrl_.js";import"./Calendar-DwcNFNf3.js";import"./startOfToday-C7FXxczV.js";import"./parse-BfiLtjV-.js";const b={title:"Components/Forms/Radio",component:r,tags:["autodocs"],parameters:{controls:{sort:"requiredFirst"}}},e={render:()=>o.jsx(r,{onSubmit:()=>{},schema:{},initialValues:s,children:o.jsx(r.Radio,{label:"How many of these shows did you watch regularly?",name:"regularviewer",options:[{name:"1",label:"1"}]})})},s={firstname:"ish",description:" sample description",regularviewer:"2",location:"Carleton University, Colonel By Drive, Ottawa, ON, Canada",favsitcom:"familymatter",favcharacters:["kellykapowski","carltonbanks"]};var a,t,i;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: () => <Form onSubmit={() => undefined} schema={{}} initialValues={InitialValues}>
+      <Form.Radio label="How many of these shows did you watch regularly?" name="regularviewer" options={[{
+      name: '1',
+      label: '1'
+    }]} />
+    </Form>
+}`,...(i=(t=e.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,b as default};
