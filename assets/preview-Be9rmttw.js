5cb6ed2c38532dc1514e1a427be4ebaddf855095
troychaplin <troy.chaplin@gmail.com>
2024-02-26 01:31:03 +0000

    Deploying to gh-pages from @ troychaplin/ui-testing@8167e5e562db99b61fc0673a4572981136e9937a 🚀

--- assets/preview-Be9rmttw.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-6ZPFLJfj.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-N5XQMW0y.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-N5XQMW0y.js","./iframe-6ZPFLJfj.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-CqY46Rbi.js","./index-Bw8VTzHM.js","./index-yajTA2Yq.js","./_baseIsEqual-C0VLQfVQ.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
