7c095356c381192e7c97325d70a9997b7291f3bc
Troy Chaplin <troy.chaplin@gmail.com>
2024-02-26 08:34:44 -0500

    change: files

--- lib/components/ButtonGroup/ButtonGroup.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export interface ButtonGroupProps {
-  children: React.ReactNode
-}
-
-export const ButtonGroup = ({ children }: ButtonGroupProps) => {
-  return <div className="flex flex-wrap gap-5 cu-buttongroup md:flex-1">{children}</div>
-}
